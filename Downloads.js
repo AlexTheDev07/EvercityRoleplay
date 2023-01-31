00fb1dee61e9936611d701395497bfbaab808a1c
Alexツ <54591924+AlexTheDev07@users.noreply.github.com>
2023-01-31 16:39:02 +0100

    Update

--- Downloads.js
+++ Downloads.js
@@ -1,3 +1,9 @@
 function BFP() {
-  location.href = "🌟  LightStudios 🌟.rbxmx";
+  alert("Could not download the Plugin. Please contact support in our discord server!")
+  location.href = "";
+}
+
+function LPA() {
+  alert("Could not download the App. Please contact support in our discord server!")
+  location.href = "";
 }
